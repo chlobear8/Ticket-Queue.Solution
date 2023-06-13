6a052f9ed30350cfabfe3b407a5737cea2dfe323
Chloe ONeil <chloesporri@yahoo.com>
2023-06-12 19:58:25 -0700

    refactor control code to implement actions

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,6 +6,7 @@ import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
 import Ticket from './Ticket';
 import PropTypes from "prop-types";
+import * as a from './../actions';
 
 class TicketControl extends React.Component {
 
@@ -20,19 +21,10 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: 'ADD TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue
-    }
-      dispatch(action); 
-      const action2 = {
-        type: 'TOGGLE_FORM'
-      }
-      dispatch(action2);              
+    const action = a.addTicket(newTicket);
+    dispatch(action);
+    const action2 = a.toggleForm();
+    dispatch(action2);         
       //this.setState({formVisibleOnPage: false });
   }
 
@@ -45,9 +37,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
       //this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage,
       //}));
@@ -61,10 +51,7 @@ class TicketControl extends React.Component {
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE TICKET',
-      id: id
-    }
+    const action = a.deleteTicket(id);
     dispatch(action);
     this.setState({
       selectedTicket: null
@@ -78,14 +65,7 @@ class TicketControl extends React.Component {
 
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue
-    }
+    const action = a.addTicket(ticketToEdit);
     dispatch(action);
     this.setState({
               editing: false,
