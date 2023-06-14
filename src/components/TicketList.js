36c3b1bc6931830d4b3f05c3774a55e3db0937cb
Chloe ONeil <chloesporri@yahoo.com>
2023-06-13 19:24:04 -0700

    add formattedWaitTime to TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -11,11 +11,12 @@ function TicketList(props) {
       names={ticket.names}
       location={ticket.location}
       issue={ticket.issue}
+      formattedWaitTime={ticket.formattedWaitTime}
       id={ticket.id}
       key={ticket.id} />
     )}
     </React.Fragment>
-  )
+  );
 }
 
 TicketList.propTypes = {
