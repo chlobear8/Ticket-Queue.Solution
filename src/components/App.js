35c881ed2cadcfd9afec0d73ca19704f4ba345aa
Chloe ONeil <chloesporri@yahoo.com>
2023-07-02 14:08:53 -0700

    push toggle light/dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [theme, setTheme] = useState(themes.light);
 
+  document.body.style.backgroundColor = theme.backgroundColor;
+  document.body.style.color = theme.textColor;
+  
   function toggleTheme() {
     setTheme(theme =>
       theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
